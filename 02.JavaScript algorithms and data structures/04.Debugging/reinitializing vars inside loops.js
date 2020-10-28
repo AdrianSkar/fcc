90bbb7abe49d36cb3e95a1d5d87d506bec52f2e6
AdrianSkar <adrian@adrianskar.com>
2020-10-28 18:00:49 +0100

    end section [02.04]

---
+++ 02.JavaScript algorithms and data structures/04.Debugging/reinitializing vars inside loops.js
@@ -0,0 +1,22 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/debugging/use-caution-when-reinitializing-variables-inside-a-loop
+
+function zeroArray(m, n) {
+	// Creates a 2-D array with m rows and n columns of zeroes
+	let newArray = [];
+	for (let i = 0; i < m; i++) {
+		let row = [];
+		// Adds the m-th row into newArray
+
+		for (let j = 0; j < n; j++) {
+			// Pushes n zeroes into the current row to create the columns
+			row.push(0);
+		}
+		// Pushes the current row, which now has n zeroes in it, to the array
+		newArray.push(row);
+		row = [];
+	}
+	return newArray;
+}
+
+let matrix = zeroArray(4, 5);
+console.log(matrix);
